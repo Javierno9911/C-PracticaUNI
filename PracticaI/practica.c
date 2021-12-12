c786a45dcb076e2906ca9760d0a486a18d38cce4
Javierno9911 <47241759+Javierno9911@users.noreply.github.com>
2021-12-12 00:28:40 -1100

    Add files via upload

---
+++ PracticaI/practica.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]){
+
+};
+
+void VolcarCACHE(T_CACHE_LINE *tbl){
+    
+};
+
+void ParsearDireccion(unsigned int addr, int *ETQ, int
+*palabra, int *linea, int *bloque){
+    
+};
+
+void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ,
+int linea, int bloque){
+    
+};
+
+
+int main(){
+    int TAM_LINEA;
+    int T_CACHE_LINE;
+    int NUM_FILAS;
+    printf("HOLA BB");
+    
+    return 0;
+}
